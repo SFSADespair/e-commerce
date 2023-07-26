fc48fced6bbdd483d9a9b3b5896533fa75c46cda
SFSADespair <tyrell.snyders@gmail.com>
2023-07-26 21:57:06 +0200

    Added schema, api and service for add-product

---
+++ src/app/api/admin/add-product/route.js
@@ -0,0 +1,65 @@
+import connectDB from "@/DB"
+import Product from "@/Models/product";
+import Joi from "joi";
+import { NextResponse } from "next/server";
+
+const AddNewProductSchema = Joi.object({
+    name: Joi.string().required(),
+    description: Joi.string().required(),
+    price: Joi.number().required(),
+    category: Joi.string().required(),
+    sizes: Joi.array().required(),
+    deliveryInfo: Joi.string().required(),
+    onSale: Joi.string().required(),
+    priceDrop: Joi.number().required(),
+    imageUrl: Joi.string().required()
+    
+})
+
+export const dynamic = 'dorce-dynamic'
+
+
+export async function POST(req) {
+    try {
+        await connectDB()
+
+        const user = 'admin'
+        if(user === 'admin') {
+            const data = await req.json()
+            const { 
+                name, price, description, imageUrl, category, sizes, onSale, deliveryInfo, priceDrop 
+            } = data
+            
+            const {error} = AddNewProductSchema.validate({
+                name, price, description, imageUrl, category, sizes, onSale, deliveryInfo, priceDrop 
+            })
+            if(error)
+                return NextResponse.json({
+                    success: false,
+                    message: error.details[0].message
+                })
+            
+            const newProduct = await Product.create(data)
+            if (newProduct)
+                return NextResponse.json({
+                    success: true,
+                    message: 'Product Added Successfuly'
+                }).status(201)
+            else
+                return NextResponse.json({
+                    success: false,
+                    message: 'Failed to add the product'
+                }).status(400)
+        } else {
+            return NextResponse.json({
+                success: false,
+                message: 'You are not authorized'
+            }).status(401)
+        }
+    } catch (err) {
+        return NextResponse.json({
+            success: false,
+            message: 'Something went wrong! Please try again later.'
+        }).status(500)
+    }
+}
---
+++ src/services/product/index.js
@@ -0,0 +1,20 @@
+import Cookies from "js-cookie";
+
+export const addNewProduct = async (formData) => {
+    try {
+        const res = await fetch('/api/admin/add-product', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                Authorization: `Bearer ${Cookies.get('token')}`
+            },
+            body: JSON.stringify(formData)
+        })
+
+        const data = await res.json()
+        return data
+    } catch (error) {
+        console.log(error);
+    }
+}
+
