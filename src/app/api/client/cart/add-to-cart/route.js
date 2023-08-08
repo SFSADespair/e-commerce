ef286bdd5072462bb17d799c1d187b14bdda39ed
SFSADespair <tyrell.snyders@gmail.com>
2023-08-08 18:05:39 +0200

    Added cart model for database and added backend route to add product to cart

---
+++ src/app/api/client/cart/add-to-cart/route.js
@@ -0,0 +1,65 @@
+import connectDB from "@/DB";
+import Cart from "@/Models/cart";
+import AuthUser from "@/middleware/AuthUser";
+import Joi from "joi";
+import { NextResponse } from "next/server";
+
+export const dynamic = 'force-dynamic'
+
+const AddToCart = Joi.object({
+    userID: Joi.string().required(),
+    productID: Joi.string().required()
+})
+
+export const POST = async(req) => {
+    try {
+        await connectDB()
+        const isAuth = await AuthUser(req)
+        
+        if (isAuth) {
+            const data = await req.json()
+            const { productID, userID } = data
+
+            const {error} = AddToCart.validate({ userID, productID })
+            if (error)
+                return NextResponse.json({
+                    success: false,
+                    message: error.details[0].message
+                })
+
+            const isCartItemExist = await Cart.find({
+                productID: productID,
+                userID: userID
+            })
+
+            if (isCartItemExist)
+                return NextResponse.json({
+                    success: false,
+                    message: 'Product is already in cart.'
+                })
+            
+            const saveProductToCart = await Cart.create(data)
+            
+            if (saveProductToCart)
+                return NextResponse.json({
+                    success: true,
+                    message: 'Added to card'
+                })
+            else
+                return NextResponse.json({
+                    success: false,
+                    message: 'Failed to add product to cart.'
+                })
+        } else 
+            return NextResponse.json({
+                success: false,
+                message: 'You are not authorised'
+            })
+    } catch (err) {
+        console.log(err);
+        return NextResponse.json({
+            success: false,
+            message: 'Something went wrong! Please try again later.'
+        })
+    }
+}
