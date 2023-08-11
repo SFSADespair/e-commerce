0cd36dd9b8c75bce1ce0bb197bc86beb66fc4183
SFSADespair <tyrell.snyders@gmail.com>
2023-08-11 19:59:38 +0200

    Created service for the cart items backend

---
+++ src/services/cart/index.js
@@ -0,0 +1,54 @@
+import Cookies from "js-cookie"
+
+export const addToCart = async(formData) => {
+    try {
+        const res = await fetch('/api/client/cart/add-to-cart', {
+            method: 'POST',
+            cache: 'no-store',
+            headers: {
+                'content-type': 'application/json',
+                Authorization: `Bearer ${Cookies.get('token')}`
+            },
+            body: JSON.stringify(formData)
+        })
+
+        const data = await res.json() 
+        return data
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+export const getCartItems = async(id) => {
+    try {
+        const res = await fetch(`api/client/cart/cart-items/?id=${id}`, {
+            method: 'GET',
+            cache: 'no-store',
+            headers: {
+                Authorization: `Bearer ${Cookies.get('token')}`
+            }
+        })
+
+        const data = await res.json()
+        return data
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export const deleteCartItem = async(id) => {
+    try {
+        const res = await fetch(`/api/client/cart/delete-from-cart/?id=${id}`, {
+            method: 'DELETE',
+            cache: 'no-store',
+            headers: {
+                Authorization: `Bearer ${Cookies.get('token')}`
+            }
+        })
+
+        const data = res.json()
+        return data
+    } catch (e) {
+        console.log(e);
+    }
+}
