e1e852e18c0efca8a7b51bd6a47b1829658bb558
SFSADespair <tyrell.snyders@gmail.com>
2023-08-13 19:48:32 +0200

    Added services for the address routes

---
+++ src/services/address/index.js
@@ -0,0 +1,74 @@
+import Cookies from "js-cookie"
+import { Cookie } from "next/font/google"
+
+const URL = 'http://localhost:3000'
+
+export const addAddress = async(formData) => {
+    try {
+        const res = await fetch('/api/address/add-address', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                Authorization: `Bearer ${Cookies.get('token')}`
+            },
+            body: JSON.stringify(formData)
+        })
+
+        const data = await res.json()
+        return data
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+export const getAddressList = async(id) => {
+    try {
+        const res = await fetch(`${URL}/api/address/get-all-address?id=${id}`, {
+            method: 'GET',
+            cache: 'no-store',
+            headers: {
+                Authorization: `Bearer ${Cookies.get('token')}`
+            }
+        })
+
+        const data = await res.json()
+        return data
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+export const updateAddress = async(formData) => {
+    try {
+        const res = await fetch('/api/address/update-address', {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json',
+                Authorization: `Bearer ${Cookies.get('token')}`
+            },
+            body: JSON.stringify(formData)
+        })
+
+        const data = res.json()
+        return data
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+export const deleteAddress = async(id) => {
+    try {
+        const res = await fetch(`/api/address/delete-address?id=${id}`, {
+            method: 'DELETE',
+            cache: 'no-store',
+            headers: {
+                Authorization: `Bearer ${Cookies.get('token')}`
+            }
+        })
+
+        const data = res.json()
+        return data
+    } catch (e) {
+        console.log(e)
+    }
+}
