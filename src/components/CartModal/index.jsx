757af4c6bbc1863750998854f027afccc31341fb
SFSADespair <tyrell.snyders@gmail.com>
2023-08-20 17:10:16 +0200

    Fix2 INVALID_IMAGE_OPTIMIZE_REQUEST

--- src/components/CartModal/index.jsx
+++ src/components/CartModal/index.jsx
@@ -44,7 +44,7 @@ const MainContent = ({cartItems, componentLevelLoader, setComponentLevelLoader, 
                             cartItems.map(item => (
                                 <li key={item.id} className="flex py-6">
                                     <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-                                        <Image 
+                                        <img 
                                             src={item && item.productID && item.productID.imageUrl}
                                             height={150}
                                             width={300}
